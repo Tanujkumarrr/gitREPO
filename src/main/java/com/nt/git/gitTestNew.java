303235a594f07e9bc47d91748590f263dcaa9173
prahalladjena1075 <prahalladjena1075@gmail.com>
2025-05-07 18:23:00 +0530

    i added ji

--- src/main/java/com/nt/git/gitTestNew.java
+++ src/main/java/com/nt/git/gitTestNew.java
@@ -3,7 +3,7 @@ package com.nt.git;
 public class gitTestNew {
 
 	public static void main(String[] args) {
-		System.out.println("tanuj papa hai");
+		System.out.println("tanuj papa ji hai");
 
 	}
 
